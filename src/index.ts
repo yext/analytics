1dea90a2f0942eabaafb0fc93cc25ec50fad6dd9
cea2aj <42848445+cea2aj@users.noreply.github.com>
2022-03-14 11:05:48 -0400

    Add README (#19)
    Add README
    
    The README is structured similarly to the one from Answers Core
    
    J=SLAP-1946
    TEST=manual

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { AnalyticsConfig } from './models/AnalyticsConfig';
 import { AnalyticsReporter } from './infra/AnalyticsReporter';
 import { HttpRequester } from './infra/HttpRequester';
-import { AnalyticsService } from './services/AnalyticsService';
+import { AnalyticsService } from './services';
 
 /**
  * The entrypoint to the analytics library.
---
+++ src/services/index.ts
@@ -0,0 +1,2 @@
+export { AnalyticsService } from './AnalyticsService';
+export { HttpRequesterService } from './HttpRequesterService';
