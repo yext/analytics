5859a0b2ce08a68e5fe42ee1f02fe88a78195423
Sara Wong <sawong@yext.com>
2023-08-21 16:17:38 -0400

    EventsSDK: create AnalyticsEventsService
    Create AnalyticsEventsService interface.
    
    J=FUS-5868
    TEST=auto

---
+++ src/AnalyticsEventsService.ts
@@ -0,0 +1,26 @@
+import {AnalyticsConfig} from "./AnalyticsConfig";
+import {EventAPIResponse, EventPayload} from "./models";
+
+export interface AnalyticsEventService {
+
+    constructor(config: AnalyticsConfig);
+
+    /**
+     * Creates a new AnalyticsEventService with the following values defined
+     * as the default when making a report.
+     *
+     * @param payload - desired values to be applied. The new payload will
+     * override any overlapping values.
+     */
+    with(payload: EventPayload): AnalyticsEventService;
+
+    /**
+     * Reports an analytics event. Operand will throw an error if the request
+     * fails. The request will be sent via Beacon API so that requests are
+     * non-blocking.
+     *
+     * @param payload - desired values to be applied. The new payload will
+     * override any overlapping values.
+     */
+    report(payload: object): Promise<EventAPIResponse>;
+};
