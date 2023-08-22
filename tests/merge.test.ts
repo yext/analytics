ebdc7f332ad82a11dd5f4284b166b00403428b60
Ethan Jaffee <ejaffee@yext.com>
2023-08-22 14:25:39 -0400

    fix comment and var names, add original uneffected check

--- tests/merge.test.ts
+++ tests/merge.test.ts
@@ -1,100 +1,110 @@
 import { EventPayload } from '../src/EventPayload';
 import { merge } from '../src/merge';
 
-describe('Merge Function', () => {
-    it('should merge example 1 correctly', () => {
-        const event1: EventPayload = {
-            action: 'APPLY',
-            bot: false,
-            locale: 'en_US',
-        };
+describe('Merge Function Test', () => {
+  it('should merge primitives values correctly', () => {
+    const event1: EventPayload = {
+      action: 'APPLY',
+      bot: false,
+      locale: 'en_US',
+    };
 
-        const event2: Record<string, unknown> = {
-            label: 'label',
-            bot: null,
-            locale: 'en_UK',
-        };
+    const uneffectedEvent1: EventPayload = {...event1};
 
-        const result = merge(event1, event2);
+    const event2: Record<string, unknown> = {
+      label: 'label',
+      bot: null,
+      locale: 'en_UK',
+    };
 
-        const expected: EventPayload = {
-            label: 'label',
-            locale: 'en_UK',
-            action: 'APPLY'
-        };
-        expect(result).toEqual(expected);
-    });
+    const result = merge(event1, event2);
 
-    it('should merge example 2 correctly', () => {
-        const event1: EventPayload = {
-            action: 'APPLY',
-            chat: {
-                botId: 'botId',
-            }
-        };
+    const expected: EventPayload = {
+      label: 'label',
+      locale: 'en_UK',
+      action: 'APPLY'
+    };
+    expect(result).toEqual(expected);
+    expect(event1).toEqual(uneffectedEvent1); // confirm event1 uneffected
 
-        const event2: Record<string, unknown> = {
-            action: 'APPLY',
-            chat: {
-                responseId: 'responseId',
-            }
-        };
+  });
 
-        const result = merge(event1, event2);
+  it('should merge objects with no overlap correctly', () => {
+    const event1: EventPayload = {
+      action: 'APPLY',
+      chat: {
+        botId: 'botId',
+      }
+    };
 
-        const expected: EventPayload = {
-            action: 'APPLY',
-            chat: {
-                botId: 'botId',
-                responseId: 'responseId',
-            }
-        };
+    const uneffectedEvent1: EventPayload = {...event1};
 
-        expect(result).toEqual(expected);
-    });
+    const event2: Record<string, unknown> = {
+      action: 'APPLY',
+      chat: {
+        responseId: 'responseId',
+      }
+    };
 
-    it('should merge example 3 correctly', () => {
-        const event1: EventPayload = {
-            action: 'APPLY',
-            browserAgent: {
-                os: 'MacOS',
-                browser: 'Chrome',
-                device: 'MacBook Pro',
-            },
-            ip: {
-                address: '0.0.0.0',
-            },
-        };
+    const result = merge(event1, event2);
 
-        const event2: Record<string, unknown> = {
-            action: 'ADD_TO_CART',
-            browserAgent: {
-                os: undefined,
-                browser: 'Safari',
-                device: 'MacBook Air',
-                deviceClass: 'Desktop',
-            },
-            ip: {
-                address: '0.0.0.1',
-                algorithm: 'hash',
-            },
-        };
+    const expected: EventPayload = {
+      action: 'APPLY',
+      chat: {
+        botId: 'botId',
+        responseId: 'responseId',
+      }
+    };
 
-        const result = merge(event1, event2);
+    expect(result).toEqual(expected);
+    expect(event1).toEqual(uneffectedEvent1); // confirm event1 uneffected
+  });
 
-        const expected: EventPayload = {
-            action: 'ADD_TO_CART',
-            browserAgent: {
-                browser: 'Safari',
-                device: 'MacBook Air',
-                deviceClass: 'Desktop',
-            },
-            ip: {
-                address: '0.0.0.1',
-                algorithm: 'hash',
-            },
-        };
+  it('should merge primitives and objects (with overlap) correctly', () => {
+    const event1: EventPayload = {
+      action: 'APPLY',
+      browserAgent: {
+        os: 'MacOS',
+        browser: 'Chrome',
+        device: 'MacBook Pro',
+      },
+      ip: {
+        address: '0.0.0.0',
+      },
+    };
 
-        expect(result).toEqual(expected);
-    });
+    const uneffectedEvent1: EventPayload = {...event1};
+
+    const event2: Record<string, unknown> = {
+      action: 'ADD_TO_CART',
+      browserAgent: {
+        os: undefined,
+        browser: 'Safari',
+        device: 'MacBook Air',
+        deviceClass: 'Desktop',
+      },
+      ip: {
+        address: '0.0.0.1',
+        algorithm: 'hash',
+      },
+    };
+
+    const result = merge(event1, event2);
+
+    const expected: EventPayload = {
+      action: 'ADD_TO_CART',
+      browserAgent: {
+        browser: 'Safari',
+        device: 'MacBook Air',
+        deviceClass: 'Desktop',
+      },
+      ip: {
+        address: '0.0.0.1',
+        algorithm: 'hash',
+      },
+    };
+
+    expect(result).toEqual(expected);
+    expect(event1).toEqual(uneffectedEvent1); // confirm event1 uneffected
+  });
 });
