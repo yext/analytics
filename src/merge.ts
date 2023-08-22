eaa3837ea2d75f42f05b3f3ac5a6d1fc576daaec
Ethan Jaffee <ejaffee@yext.com>
2023-08-22 17:31:45 -0400

    add merging conventions to comment, make newValues a partial

--- src/merge.ts
+++ src/merge.ts
@@ -1,14 +1,21 @@
 import { EventPayload } from './EventPayload';
 
-/** Used for merging request JSON into an existing EventPayload.
+/**
+ * Used for merging request JSON into an existing EventPayload.
  * Returns a new EventPayload, with the original object uneffected.
+ * The merge occurs using the following conventions:
+ *
+ *  Merging an existing key with null/undefined deletes the key.
+ *  Merging an existing key with non-null value update the value.
+ *  Merging a non-existing key adds the key/value.
+ *  When the value is an object, the above is applied recursivley.
  */
-export const merge = (original: EventPayload, newValues: Record<string, unknown>): EventPayload => {
+export const merge = (original: EventPayload, newValues: Partial<Record<keyof EventPayload, unknown>>): EventPayload => {
   const copy: Record<string, unknown> = { ...original };
 
   for (const key in newValues) {
     if (newValues.hasOwnProperty(key)) {
-      const value = newValues[key];
+      const value = (newValues as Record<string, unknown>)[key];
       if (value === null || value === undefined) {
         delete copy[key];
       } else if (typeof value !== 'object') {
