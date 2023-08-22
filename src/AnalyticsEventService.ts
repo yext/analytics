7ccbd2200f667c67fb5a661e6c71c702c65a5bf0
Sara Wong <sawong@yext.com>
2023-08-22 15:19:43 -0400

    Update method description

--- src/AnalyticsEventService.ts
+++ src/AnalyticsEventService.ts
@@ -13,13 +13,13 @@ export interface AnalyticsEventService {
 
     /**
      * Reports an analytics event. Operand will throw an error if the request
-     * fails. The request will be sent via Beacon API so that requests are
-     * non-blocking. If debug mode is enabled in the config, the request will
-     * instead be sent via fetch and a Promise of an EventAPIResponse will be
-     * returned.
+     * fails. The request will default to being sent via fetch, with keepalive set to true,
+     * if supported by the browser, and a Promise of an EventAPIResponse will be returned.
+     * Otherwise, the request will be sent via the Beacon API, unless the forceFetch flag is
+     * toggled, and a boolean will be returned.
      *
      * @param payload - desired values to be applied. The new payload will
      * override any overlapping values.
      */
-    report(payload: object): boolean | Promise<EventAPIResponse>;
+    report(payload: object): Promise<EventAPIResponse> | boolean;
 };
