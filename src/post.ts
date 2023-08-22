82be02afb867c8001b3ab8653ef52c7379e64c52
Sara Wong <sawong@yext.com>
2023-08-22 16:20:07 -0400

    Add description

--- src/post.ts
+++ src/post.ts
@@ -1,5 +1,15 @@
 import {EventPayload} from './EventPayload';
 
+/**
+ * Used by the AnalyticsEventReport report() method to send an analytics event request via the
+ * Beacon API.
+ *
+ * todo: Update this method to default to using fetch + keepalive unless not supported and
+ * forceFetch is not set to true. Also update the description above.
+ *
+ * @param url URL that the request will be sent to
+ * @param body the EventPayload object
+ */
 const post = (
     url: string,
     body: EventPayload
