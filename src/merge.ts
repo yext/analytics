d26bcb280126c9ae6bab799eeb40240202307003
Ethan Jaffee <ejaffee@yext.com>
2023-08-22 14:25:39 -0400

    fix comment and var names, add original uneffected check

--- src/merge.ts
+++ src/merge.ts
@@ -1,25 +1,27 @@
 import { EventPayload } from './EventPayload';
 
-/** Used for merging request JSON into an existing EventPayload. Returns a new EventPayload object */
-export const merge = (json1: EventPayload, json2: Record<string, unknown>): EventPayload => {
-    const copy: Record<string, unknown> = { ...json1 };
+/** Used for merging request JSON into an existing EventPayload.
+ * Returns a new EventPayload, with the original object uneffected.
+ */
+export const merge = (original: EventPayload, newValues: Record<string, unknown>): EventPayload => {
+  const copy: Record<string, unknown> = { ...original };
 
-    for (const key in json2) {
-        if (json2.hasOwnProperty(key)) {
-            const value = json2[key];
-            if (value === null || value === undefined) {
-                delete copy[key];
-            } else if (typeof value !== 'object') {
-                copy[key] = value;
-            } else {
-                copy[key] = merge(copy[key] as EventPayload, value as Record<string, unknown>);
-            }
-        }
+  for (const key in newValues) {
+    if (newValues.hasOwnProperty(key)) {
+      const value = newValues[key];
+      if (value === null || value === undefined) {
+        delete copy[key];
+      } else if (typeof value !== 'object') {
+        copy[key] = value;
+      } else {
+        copy[key] = merge(copy[key] as EventPayload, value as Record<string, unknown>);
+      }
     }
+  }
 
-    const result: EventPayload = {
-        ...copy,
-        action: copy.action as EventPayload['action']
-    };
-    return result;
+  const result: EventPayload = {
+    ...copy,
+    action: copy.action as EventPayload['action']
+  };
+  return result;
 };
