d518a7772e0f4a82fa544030e49f59adc738abd2
Sara Wong <sawong@yext.com>
2023-08-23 15:07:58 -0400

    EventsSDK: Update post() to use fetch+keepalive
    Update the post() util function to use fetch and keepAlive by default,
    unless the forceFetch flag is false and the browser does not support
    keepAlive (it should be supported by all browsers except Firefox and
    Firefox for Android).
    
    J=FUS-5878
    TEST=auto
    
    Wrote jest tests and ran npm run test.

--- src/post.ts
+++ src/post.ts
@@ -1,20 +1,27 @@
 import {EventPayload} from './EventPayload';
 
 /**
- * Used by the AnalyticsEventReport report() method to send an analytics event request via the
- * Beacon API.
- *
- * todo: Update this method to default to using fetch + keepalive unless not supported and
- * forceFetch is not set to true. Also update the description above.
+ * Used by the AnalyticsEventReport report() method to send an analytics event request via
+ * fetch, with the keepalive option set to true. If forceFetch is false and keepalive is not
+ * supported by the browser i.e. Firefox, the request is sent via the Beacon API.
  *
  * @param url URL that the request will be sent to
  * @param body the EventPayload object
+ * @param forceFetch if true, it will force the browser to call fetch instead of sendBeacon,
+ * regardless of whether keepAlive is supported
  */
-const post = (
+const post = async (
     url: string,
-    body: EventPayload
+    body: EventPayload,
+    forceFetch: boolean,
 ) => {
-    return navigator.sendBeacon(url, JSON.stringify(body));
+    const agent = body.browserAgent;
+    // fetch + keepalive is not supported in Firefox
+    let useBeacon = (agent && agent.browser && agent.browser.startsWith('Firefox')) || (navigator.userAgent && navigator.userAgent.includes('Firefox'));
+    if (!forceFetch && useBeacon) {
+        return navigator.sendBeacon(url, JSON.stringify(body));
+    }
+    return fetch(url, {method: 'POST', body: JSON.stringify(body), keepalive: true});
 }
 
 export default post;
