2ff60edf4e4dc919eb772bcbc36ea9700905c281
Ethan Jaffee <ejaffee@yext.com>
2023-08-25 14:05:27 -0400

    EventsApi: Implement index.ts
    Implement index.ts by exporting an analytics object that takes in
    a config, creates a reporter, and returns a service. This is the only
    thing we are exporting in the sdk.
    
    J=FUS-5974
    R=mtian, abenno
    TEST=manual
    
    test-site to be updated to use new reporter.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export * from '.';
+import { AnalyticsConfig } from './AnalyticsConfig';
+import { AnalyticsEventReporter } from './AnalyticsEventReporter';
+import { AnalyticsEventService } from './AnalyticsEventService';
+
+/**
+ * The Yext Analytics Events SDK
+ * @beta
+ */
+const analytics = (config: AnalyticsConfig): AnalyticsEventService => {
+  return new AnalyticsEventReporter(config);
+};
+
+export default analytics;
