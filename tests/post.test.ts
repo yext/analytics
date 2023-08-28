22a249bd73d3eda192f868dca44c87b3048e22ce
Ethan Jaffee <ejaffee@yext.com>
2023-08-28 14:23:07 -0400

    seperate post by caller, update tests, test useBeacon

--- tests/post.test.ts
+++ tests/post.test.ts
@@ -1,78 +1,78 @@
 import { EventPayload } from '../src/EventPayload';
 import { EventAPIResponse } from '../src/EventAPIResponse';
-import { post, useBeacon } from '../src/post';
+import { postWithBeacon, postWithFetch, useBeacon } from '../src/post';
 const fetchMock = /** @type {FetchMockJest} */ (require('fetch-mock-jest'));
 
 describe('Test post util function', () => {
 
-    beforeEach(() => {
-        const navigator = { userAgent: 'Firefox', sendBeacon: () => { return true }};
-        Object.defineProperty(window, 'navigator', { value: navigator, writable: true});
-    });
+  beforeEach(() => {
+    const navigator = { userAgent: 'Firefox', sendBeacon: () => { return true; }};
+    Object.defineProperty(window, 'navigator', { value: navigator, writable: true});
+  });
 
-    const eventResponseA: EventAPIResponse = {
-        id: 'event-id-A'
-    }
+  const eventResponseA: EventAPIResponse = {
+    id: 'event-id-A'
+  };
 
-    const eventPayloadA: EventPayload = {
-        action: 'ADD_TO_CART',
-        locale: 'en_US',
-    };
+  const eventPayloadA: EventPayload = {
+    action: 'ADD_TO_CART',
+    locale: 'en_US',
+  };
 
-    const url = 'https://dev.us.yextevents.com/accounts/me/events';
+  const url = 'https://dev.us.yextevents.com/accounts/me/events';
 
-    const optionsA = {method: 'POST', body: JSON.stringify(eventPayloadA), keepalive: true};
+  const optionsA = {method: 'POST', body: JSON.stringify(eventPayloadA), keepalive: true};
 
-    fetchMock.post(url, JSON.stringify(eventResponseA));
+  fetchMock.post(url, JSON.stringify(eventResponseA));
 
-    it('should use fetch', () => {
-        // userAgent is Firefox but forceFetch = true
-        let result = useBeacon(eventPayloadA, true);
-        expect(result).toBe(false);
+  it('should use fetch', () => {
+    // userAgent is Firefox but forceFetch = true
+    let result = useBeacon(eventPayloadA, true);
+    expect(result).toBe(false);
 
-        const navigator = { userAgent: 'Chrome' };
-        Object.defineProperty(window, 'navigator', { value: navigator, writable: true});
+    const navigator = { userAgent: 'Chrome' };
+    Object.defineProperty(window, 'navigator', { value: navigator, writable: true});
 
-        // userAgent is Chrome and forceFetch = false
-        result = useBeacon(eventPayloadA, false);
-        expect(result).toBe(false);
+    // userAgent is Chrome and forceFetch = false
+    result = useBeacon(eventPayloadA, false);
+    expect(result).toBe(false);
 
-        // userAgent is Chrome and forceFetch = true
-        result = useBeacon(eventPayloadA, true);
-        expect(result).toBe(false);
-    })
+    // userAgent is Chrome and forceFetch = true
+    result = useBeacon(eventPayloadA, true);
+    expect(result).toBe(false);
+  });
 
-    it('should use beacon', () => {
-        // userAgent is Firefox
-        let result = useBeacon(eventPayloadA, false);
-        expect(result).toBe(true);
-    })
+  it('should use beacon', () => {
+    // userAgent is Firefox
+    const result = useBeacon(eventPayloadA, false);
+    expect(result).toBe(true);
+  });
 
-    it('should send fetch request', async () => {
-        // forceFetch: true
-        await post(url, eventPayloadA, true);
-        expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events').length).toEqual(1);
-        expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events')[0][1]).toEqual(optionsA);
+  it('should send fetch request', async () => {
+    // forceFetch: true
+    await postWithFetch(url, eventPayloadA);
+    expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events').length).toEqual(1);
+    expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events')[0][1]).toEqual(optionsA);
 
-        // forceFetch: false, but browser is not Firefox
-        const navigator = { userAgent: 'Chrome', sendBeacon: () => { return true }};
-        Object.defineProperty(window, 'navigator', { value: navigator, writable: true});
-        await post(url, eventPayloadA, false);
-        expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events').length).toEqual(2);
-        expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events')[1][1]).toEqual(optionsA);
+    // forceFetch: false, but browser is not Firefox
+    const navigator = { userAgent: 'Chrome', sendBeacon: () => { return true; }};
+    Object.defineProperty(window, 'navigator', { value: navigator, writable: true});
+    await postWithFetch(url, eventPayloadA);
+    expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events').length).toEqual(2);
+    expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events')[1][1]).toEqual(optionsA);
 
-        // forceFetch: true, browser is not Firefox
-        await post(url, eventPayloadA, true);
-        expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events').length).toEqual(3);
-        expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events')[2][1]).toEqual(optionsA);
+    // forceFetch: true, browser is not Firefox
+    await postWithFetch(url, eventPayloadA);
+    expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events').length).toEqual(3);
+    expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events')[2][1]).toEqual(optionsA);
 
-        fetchMock.mockClear();
-    })
+    fetchMock.mockClear();
+  });
 
-    it('should send beacon request', async () => {
-        // forceFetch: false and browser defined in navigator is Firefox
-        let result = await post(url, eventPayloadA, false);
-        expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events').length).toEqual(0);
-        expect(result).toBe(true);
-    })
-})
+  it('should send beacon request', async () => {
+    // forceFetch: false and browser defined in navigator is Firefox
+    const result =await postWithBeacon(url, eventPayloadA);
+    expect(fetchMock.calls('https://dev.us.yextevents.com/accounts/me/events').length).toEqual(0);
+    expect(result).toBe(true);
+  });
+});
