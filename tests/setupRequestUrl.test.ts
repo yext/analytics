ccbc20b6077a904c983e5c948acac44dfdce8714
Ethan Jaffee <ejaffee@yext.com>
2023-08-28 14:22:09 -0400

    update setupRequestUrl tests

--- tests/setupRequestUrl.test.ts
+++ tests/setupRequestUrl.test.ts
@@ -1,7 +1,6 @@
 import { EnvironmentEnum } from '../src/Environment';
-import { EventPayload } from '../src/EventPayload';
 import { RegionEnum } from '../src/Region';
-import setUpRequestUrl, { setupRequestUrl } from '../src/setupRequestUrl';
+import { setupRequestUrl } from '../src/setupRequestUrl';
 
 describe('setUpRequestUrl Test', () => {
   it('should set url correctly for env: prod, region: us', () => {
@@ -18,16 +17,37 @@ describe('setUpRequestUrl Test', () => {
     expect(resultUrl).toEqual(expectedUrl);
   });
 
-  it('should set url correctly for env: prod, region: us', () => {
+  it('should set url correctly for env: sbx, region: us', () => {
     const resultUrl = setupRequestUrl(EnvironmentEnum.Sandbox, RegionEnum.US);
     const expectedUrl = 'https://sbx.us.yextevents.com/accounts/me/events';
 
     expect(resultUrl).toEqual(expectedUrl);
   });
 
-  it('should set url correctly for env: prod, region: us', () => {
+  it('should set url correctly for env: sbx, region: eu', () => {
     const resultUrl = setupRequestUrl(EnvironmentEnum.Sandbox, RegionEnum.EU);
     const expectedUrl = 'https://sbx.eu.yextevents.com/accounts/me/events';
+
+    expect(resultUrl).toEqual(expectedUrl);
+  });
+
+  it('should set url correctly with no arguments passed in', () => {
+    const resultUrl = setupRequestUrl();
+    const expectedUrl = 'https://us.yextevents.com/accounts/me/events';
+
+    expect(resultUrl).toEqual(expectedUrl);
+  });
+
+  it('should set url correctly with no env argument passed in', () => {
+    const resultUrl = setupRequestUrl(undefined, RegionEnum.EU);
+    const expectedUrl = 'https://eu.yextevents.com/accounts/me/events';
+
+    expect(resultUrl).toEqual(expectedUrl);
+  });
+
+  it('should set url correctly with no region argument passed in', () => {
+    const resultUrl = setupRequestUrl(EnvironmentEnum.Sandbox, undefined);
+    const expectedUrl = 'https://sbx.us.yextevents.com/accounts/me/events';
 
     expect(resultUrl).toEqual(expectedUrl);
   });
