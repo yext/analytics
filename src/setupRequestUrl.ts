49e9d0727c726d979a365205bdda457558b51b6d
Ethan Jaffee <ejaffee@yext.com>
2023-08-28 16:20:21 -0400

    make urlBase global

--- src/setupRequestUrl.ts
+++ src/setupRequestUrl.ts
@@ -1,7 +1,8 @@
 import { Environment, EnvironmentEnum } from './Environment';
 import { Region } from './Region';
 
+const urlBase = 'yextevents.com/accounts/me/events';
+
 export function setupRequestUrl(env?: Environment, region?: Region): string {
-  const urlBase = 'yextevents.com/accounts/me/events';
   return 'https://' + (env === EnvironmentEnum.Sandbox ? 'sbx.' : '') + (region ?? 'us') + '.' + urlBase;
 }
