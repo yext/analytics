dec246dd3812ba5ecefbb36bcab5dd8073097485
Ethan Jaffee <ejaffee@yext.com>
2023-08-28 14:23:07 -0400

    seperate post by caller, update tests, test useBeacon

--- src/post.ts
+++ src/post.ts
@@ -2,18 +2,24 @@ import {EventPayload} from './EventPayload';
 
 /**
  * Used by the AnalyticsEventReport report() method to send an analytics event request via
- * fetch, with the keepalive option set to true. If forceFetch is false and keepalive is not
+ * Beacon. Used when forceFetch is false and keepalive is not
  * supported by the browser i.e. Firefox, the request is sent via the Beacon API.
  *
  * @param url URL that the request will be sent to
  * @param body the EventPayload object
- * @param forceFetch if true, it will force the browser to call fetch instead of sendBeacon,
- * regardless of whether keepAlive is supported
  */
-export function post(url: string, body: EventPayload, forceFetch: boolean | undefined) {
-  if (useBeacon(body, forceFetch)) {
-    return navigator.sendBeacon(url, JSON.stringify(body));
-  }
+export function postWithBeacon(url: string, body: EventPayload): boolean {
+  return navigator.sendBeacon(url, JSON.stringify(body));
+}
+
+/**
+ * Used by the AnalyticsEventReport report() method to send an analytics event request via
+ * fetch. Used when forceFetch is true.
+ *
+ * @param url URL that the request will be sent to
+ * @param body the EventPayload object
+ */
+export function postWithFetch(url: string, body: EventPayload): Promise<Response> {
   return fetch(url, {method: 'POST', body: JSON.stringify(body), keepalive: true});
 }
 
