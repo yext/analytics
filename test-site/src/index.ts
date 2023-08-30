98b69aa4f391a97ed777138e1b9b55cfe163dbf3
Ethan Jaffee <ejaffee@yext.com>
2023-08-30 18:28:25 -0400

    add comment about firefox bug
    Change-Id: I6f724bd350a321da0d98c0237d5ce726049d08e9

--- test-site/src/index.ts
+++ test-site/src/index.ts
@@ -1,6 +1,13 @@
 import { analytics } from '@yext/analytics';
 
-
+/** Note: there is currently a bug when sending requests in FireFox where the Network tab
+ * will show NS_ERROR_FAILURE under transferred even though the response is successful 
+ * and Yext successfuly recieved the data. 
+ * For Yext employees there is more info here: 
+ * https://yext.slack.com/archives/G8UMH43HP/p1693419787243609?thread_ts=1693411231.167479&cid=G8UMH43HP
+ * 
+ * We will continue investigating this make a fix if necessary. 
+*/
 const analyticsProvider = analytics({
   key: process.env.YEXT_API_KEY,
   sessionTrackingEnabled: false
