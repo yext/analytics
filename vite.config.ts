a2b2d8797eeaf4e16ab90e9b90ae77b3d1969690
Merrick Tian <mtian@yext.com>
2023-08-31 16:26:29 -0400

    Add test cdn site

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "AnalyticsSdk",
+      name: "AnalyticsSDK",
       formats: ["umd"],
       fileName: "analytics",
     }
