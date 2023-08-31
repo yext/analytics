e82190283e8f5f882357c9ee9534adbb7c75dcc0
Ethan Jaffee <ejaffee@yext.com>
2023-08-31 14:27:37 -0400

    change setupRequestUrl to throw error when env sbx & region eu passed, update tests
    Change-Id: I7dfc579d9d00133b6ef02db24800047cd2dce665

--- src/setupRequestUrl.ts
+++ src/setupRequestUrl.ts
@@ -1,8 +1,11 @@
 import { Environment, EnvironmentEnum } from './Environment';
-import { Region } from './Region';
+import { Region, RegionEnum } from './Region';
 
 const urlBase = 'yextevents.com/accounts/me/events';
 
 export function setupRequestUrl(env?: Environment, region?: Region): string {
+  if (env === EnvironmentEnum.Sandbox && region === RegionEnum.EU) {
+    throw new Error('Sandbox environment is not available in the EU region.');
+  }
   return 'https://' + (env === EnvironmentEnum.Sandbox ? 'sbx.' : '') + (region ?? 'us') + '.' + urlBase;
 }
--- tests/setupRequestUrl.test.ts
+++ tests/setupRequestUrl.test.ts
@@ -24,11 +24,14 @@ describe('setUpRequestUrl Test', () => {
     expect(resultUrl).toEqual(expectedUrl);
   });
 
-  it('should set url correctly for env: sbx, region: eu', () => {
-    const resultUrl = setupRequestUrl(EnvironmentEnum.Sandbox, RegionEnum.EU);
-    const expectedUrl = 'https://sbx.eu.yextevents.com/accounts/me/events';
-
-    expect(resultUrl).toEqual(expectedUrl);
+  it('should throw error for env: sbx, region: eu', () => {
+    try {
+      setupRequestUrl(EnvironmentEnum.Sandbox, RegionEnum.EU);
+      // Fail test if above expression doesn't throw anything.
+      expect(true).toBe(false);
+    } catch (e) {
+      expect(e.message).toBe('Sandbox environment is not available in the EU region.');
+    }
   });
 
   it('should set url correctly with no arguments passed in', () => {
