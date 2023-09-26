d6c07fdda4464638393a1a933bc36bab72ea8361
Ethan Jaffee <ejaffee@yext.com>
2023-09-25 21:10:04 -0400

    EventsSDK: Add Playwright Test
    Add playwright tests to serve as acceptance tests for this SDK. They
    will run the test site and click the buttons on three browsers,
    Chromium, Firefox, and Webkit and check for a 202 response. They can be
    run with the other unit tests using npm run test, or on their own with
    npx playwright test. The browsers can also be viewed live as they
    execute by using npx playwright test --headed.
    
    J=FUS-5909
    R=mtian, sawong
    TEST=auto
    
    Ran npm run test all pass.

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,14 +20,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry'
+    trace: 'on-first-retry',
+    video: 'on'
   },
 
   /* Configure projects for major browsers */
@@ -66,12 +67,12 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ]
+  ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'cd test-site && npm run build && npm run serve',
+    url: 'http://127.0.0.1:3030',
+    reuseExistingServer: !process.env.CI
+  }
 });
