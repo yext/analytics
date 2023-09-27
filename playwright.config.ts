bda6eb7d3e9a8530e85137d9024f0b907f91ca3f
Ethan Jaffee <ejaffee@yext.com>
2023-09-26 21:49:18 -0400

    try &

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run serve --prefix ./test-site',
-    port: 3030
+    command: 'npm run serve --prefix ./test-site &',
+    port: 3030,
+    reuseExistingServer: true
   }
 });
