afa4cb5e4bed53cd260bff35a3b6f85f4fac311a
Ethan Jaffee <ejaffee@yext.com>
2023-09-26 22:29:57 -0400

    try this

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -74,6 +74,6 @@ export default defineConfig({
     command:
       'npm run build --prefix ./test-site && npm run serve --prefix ./test-site',
     reuseExistingServer: true,
-    url: 'http://localhost:3030/'
+    port: 3030
   }
 });
