1af529e08ee5e4d042eb56e936dad98cdf8d67da
Ethan Jaffee <ejaffee@yext.com>
2023-09-28 00:26:27 -0400

    EventsSDK: Add Playwright Tests
    Add playwright tests to serve as acceptance tests for this SDK.
    They will run the test site and click the buttons on three browsers,
    Chromium, Firefox, and Webkit and check for a 202 response. They can
    be run with the other unit tests using npm run test, or on their own
    with npx playwright test. The browsers can also be viewed live as
    they execute by using npx playwright test --headed.
    
    J=FUS-5909
    R=mtian, sawong, abenno
    TEST=auto

---
+++ test-e2e/playwright.test.ts
@@ -0,0 +1,37 @@
+import { test } from '@playwright/test';
+
+test('test Fire Chat Event on Chromium, Firefox, and Webkit', async ({
+  page
+}) => {
+  const responsePromise = page.waitForResponse((res) => res.status() == 202);
+  await page.goto('/');
+  await page.click('button:has-text("Fire Chat Event")');
+  await responsePromise;
+});
+
+test('test Fire Search Event on Chromium, Firefox, and Webkit', async ({
+  page
+}) => {
+  const responsePromise = page.waitForResponse((res) => res.status() == 202);
+  await page.goto('/');
+  await page.click('button:has-text("Fire Search Event")');
+  await responsePromise;
+});
+
+test('test Fire CTA Event on Chromium, Firefox, and Webkit', async ({
+  page
+}) => {
+  const responsePromise = page.waitForResponse((res) => res.status() == 202);
+  await page.goto('/');
+  await page.click('button:has-text("Fire CTA Event")');
+  await responsePromise;
+});
+
+test('test Fire Event w/ Session Tracking on Chromium, Firefox, and Webkit', async ({
+  page
+}) => {
+  const responsePromise = page.waitForResponse((res) => res.status() == 202);
+  await page.goto('/');
+  await page.click('button:has-text("Fire Event w/ Session Tracking")');
+  await responsePromise;
+});
