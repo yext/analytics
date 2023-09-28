5c03255afaa87bf5ecd10a4e430846336c52883a
Ethan Jaffee <ejaffee@yext.com>
2023-09-27 21:23:23 -0400

    remove expect

--- test-e2e/playwright.test.ts
+++ test-e2e/playwright.test.ts
@@ -6,34 +6,7 @@ test('test Fire Chat Event on Chromium, Firefox, and Webkit', async ({
   const responsePromise = page.waitForResponse((res) => res.status() == 202 || res.status() == 200);
   await page.goto('http://localhost:3000');
   await page.click('button:has-text("Fire Chat Event")');
-  const response = await responsePromise;
-  expect(response.status()).toBe(200 || 202);
-
-
-  // await Promise.all([
-  //   page.waitForResponse((res) => res.status() == 202),
-  //   page.click('button:has-text("Fire Chat Event")')
-  // ])
-  //   .then((responses) => {
-  //     console.log(responses.at(0));
-  //     console.log(responses.at(0)?.status());
-  //     console.log(responses.at(0)?.statusText());
-  //     console.log(responses.at(0)?.ok());
-
-  //     console.log(responses.at(1));
-  //     console.log(responses.at(1)?.status());
-  //     console.log(responses.at(1)?.statusText());
-  //     console.log(responses.at(1)?.ok());
-  //     expect(responses.at(0)?.status()).toBe(202);
-  //   })
-  //   .catch((e) => {
-  //     console.log(e);
-  //     console.log(e.status());
-  //     console.log(e.statusText());
-  //     console.log(e.message());
-  //     console.log(e.name());
-  //     test.fail(e);
-  //   });
+  await responsePromise;
 });
 
 test('test Fire Search Event on Chromium, Firefox, and Webkit', async ({
