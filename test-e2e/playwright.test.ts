28f2b909535ddf471692184b693c401c81a3ba6f
Ethan Jaffee <ejaffee01@users.noreply.github.com>
2023-10-05 15:41:43 -0400

    EventsSDK: Add support for Yext Pages (#110)
    Add Yext Pages properties to the EventPayload.
    
    J=FUS-5911
    R=abenno, mtian
    TEST=auto
    
    - Updated test-site to have button for reporting Pages events.
    - Updated playwright tests (test-e2e) to hit this button and expect 202
    - Updated test-cdn to have button for Search and Pages

--- test-e2e/playwright.test.ts
+++ test-e2e/playwright.test.ts
@@ -22,6 +22,15 @@ test('test Fire Search Event on Chromium, Firefox, and Webkit', async ({
   await responsePromise;
 });
 
+test('test Fire Pages Event on Chromium, Firefox, and Webkit', async ({
+  page
+}) => {
+  const responsePromise = page.waitForResponse((res) => res.status() == 202);
+  await page.goto('/');
+  await page.click('button:has-text("Fire Pages Event")');
+  await responsePromise;
+});
+
 test('test Fire CTA Event on Chromium, Firefox, and Webkit', async ({
   page
 }) => {
--- test-site/src/index.ts
+++ test-site/src/index.ts
@@ -115,6 +115,17 @@ export function fireSearchEvent() {
   });
 }
 
+export function firePagesEvent() {
+  analyticsProvider.report({
+    action: 'WEBSITE',
+    chat: null,
+    sites: {
+      siteUid: 123456789,
+      template: 'test-template'
+    }
+  });
+}
+
 export function fireCallToActionEvent() {
   analyticsProvider.report({ action: 'CALL_TO_ACTION' });
 }
