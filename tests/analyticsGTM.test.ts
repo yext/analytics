09944a8fb19dd1b0803866045278340f41ef3b79
Ethan Jaffee <ejaffee@yext.com>
2023-11-08 13:58:46 -0500

    fix test failure

--- tests/analyticsGTM.test.ts
+++ tests/analyticsGTM.test.ts
@@ -20,8 +20,8 @@ describe('analyticsGTM', () => {
 
   it('should return a promise that resolves when payload is present', async () => {
     const mockPayload = [
-      [{ key: 'apiKey' }, { key: 'config' }],
-      [{ bot: false }, { key: 'data' }]
+      ['config', { key: 'apiKey' }],
+      ['payload', { bot: false }]
     ];
     (global as any).window['analyticsEventPayload'] = mockPayload;
 
@@ -33,26 +33,6 @@ describe('analyticsGTM', () => {
 
   it('should return a promise that rejects when analyticeEvenPayload is not present in window', async () => {
     (global as any).window['analyticsEventPayload'] = undefined;
-
-    await expect(analyticsGTM()).rejects.toEqual('No payload found.');
-  });
-
-  it('should return a promise that rejects when payload is present but config is not', async () => {
-    const mockPayload = [
-      [{ bot: false }, { key: 'data' }],
-      [null, { key: 'config' }]
-    ];
-    (global as any).window['analyticsEventPayload'] = mockPayload;
-
-    await expect(analyticsGTM()).rejects.toEqual('No config found in payload.');
-  });
-
-  it('should return a promise that rejects when config is present but payload is not', async () => {
-    const mockPayload = [
-      [{ key: 'apiKey' }, { key: 'config' }],
-      [null, { key: 'data' }]
-    ];
-    (global as any).window['analyticsEventPayload'] = mockPayload;
 
     await expect(analyticsGTM()).rejects.toEqual('No payload found.');
   });
